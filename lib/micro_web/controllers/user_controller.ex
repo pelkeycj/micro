2da11947513717cc71f1e9ec9760322f20a26354
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-09-30 20:44:02 -0400

    prettify the user views

--- lib/micro_web/controllers/user_controller.ex
+++ lib/micro_web/controllers/user_controller.ex
@@ -18,7 +18,6 @@ defmodule MicroWeb.UserController do
     case Accounts.create_user(user_params) do
       {:ok, user} ->
         conn
-        |> put_flash(:info, "User created successfully.")
         |> redirect(to: user_path(conn, :show, user))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
