9e33d769cb60abbc7ba59d2961fff8cb78ceb979
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-10-15 21:51:24 -0400

    test config

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :micro, Micro.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "micro",
-  password: "pelkey.cRU28ul25!",
+  username: "postgres",
+  password: "postgres",
   database: "micro_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
